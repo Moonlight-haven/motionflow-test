fe35b393b7254a424f99babdb1a7029e8636ce7e
Moonlight-haven <moonlight.allacct@gmail.com>
2026-05-28 08:10:35 +0100

    Update config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Motion-Flow Central Configuration
+window.firebaseConfig = {
+    apiKey: "AIzaSyCy5uQGcg1cALqFSa1xAQOL6lX8Gz5nFNU",
+    authDomain: "motion-flow-47450.firebasestorage.app",
+    databaseURL: "https://motion-flow-47450-default-rtdb.firebaseio.com",
+    projectId: "motion-flow-47450",
+    storageBucket: "motion-flow-47450.firebasestorage.app",
+    messagingSenderId: "686875529848",
+    appId: "1:686875529848:web:48edf5164aaaf938cdd38d"
+};
+
+window.APP_ID = 'motion-flow-47450';
+window.ADMIN_EMAIL = "motionflow.admins@gmail.com";
+window.ADMIN_PASSWORD = "MOTION-FLOW-ADMIN";
